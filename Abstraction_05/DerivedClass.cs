7b7db3b738c684d2d57c38379b05afab8731e896
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-01 23:05:56 +0400

    Implemented base class methods

---
+++ Abstraction_05/DerivedClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_05
+{
+    class ConcreteDerivedClass : AbstractBaseClass
+    {
+        // Override the virtual method VirtualMethod () of the base abstract class.
+        // If we do not override the virtual method, then the method from the base class will be used.
+
+        //public override void VirtualMethod()
+        //{
+        //    Console.WriteLine("DerivedClass.VirtualMethod ();");
+        //}
+
+        // Implement the abstract method AbstractMethod () of the base abstract class.
+
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("DerivedClass.AbstractMethod ();");
+        }
+    }
+}
