67f47331055c849996e2066b240d0f08603fd21c
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-01 22:32:50 +0400

    Abstraction can be achieved using abstract classes in C#.

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -5,11 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Abstraction
-   
-{
-    // Abstract class.
 
-        abstract class AbstractClass
+{/* Abstraction and encapsulation are related features in object-oriented programming.
+    * Abstraction allows making relevant information visible and encapsulation enables a programmer 
+    * to implement the desired level of abstraction.
+    * Abstraction can be achieved using abstract classes in C#. C# allows you to create abstract 
+    * classes that are used to provide a partial class implementation of an interface. 
+    * Implementation is completed when a derived class inherits from it. Abstract classes contain abstract methods, 
+    * which are implemented by the derived class. The derived classes have more specialized functionality.
+The following are some of the key points −
+You cannot create an instance of an abstract class
+You cannot declare an abstract method outside an abstract class
+When a class is declared sealed, it cannot be inherited, abstract classes cannot be declared sealed.*/
+ // Abstract class.
+
+    abstract class AbstractClass
     { 
         //public virtual void Method(){} is the same
         public abstract void Method();
