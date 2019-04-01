dabbe1b87380ac6d5f07d82f1d7f332e49e0458e
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-01 23:01:22 +0400

    An abstract can contain normal, virtual and abstract methods

---
+++ Abstraction_04/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_04
+{
+    abstract class AbstractBaseClass
+    {
+        // 1.//substitute
+        // A normal method is passed to the derived class as if it were inherited from a particular class.
+        public void SimpleMethod()
+        {
+            Console.WriteLine("AbstractBaseClass.SimpleMethod");
+        }
+
+        // 2. override
+        // The virtual method is passed to the derived class as if it were inherited from a particular class.
+        virtual public void VirtualMethod()
+        {
+            Console.WriteLine("AbstractBaseClass.VirtualMethod");
+        }
+
+        // 3. implemented
+        // Abstract method - implemented in a derived class.
+        abstract public void AbstractMethod();
+    }
+}
