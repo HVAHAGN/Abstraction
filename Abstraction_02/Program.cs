710edafa06e56f44487a78ec905ccf46efe8f07d
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-01 22:44:39 +0400

    An abstract class can be inherited from a particular class

---
+++ Abstraction_02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_02
+{
+    // An abstract class can be inherited from a particular class.
+
+    
+        // Specific class A.
+        class ConcreteClassA
+        {
+            public void Operation()
+            {
+                Console.WriteLine("ConcreteClassA.Operation");
+            }
+        }
+
+        // Abstract class.
+        abstract class AbstractClass : ConcreteClassA
+        {
+            public abstract void Method();
+        }
+
+        // Specific class B.
+        class ConcreteClassB : AbstractClass
+        {
+            public override void Method()
+            {
+                Console.WriteLine("ConcreteClassB.Method");
+            }
+        }
+
+        class program
+        {
+            static void Main()
+            {
+                AbstractClass instance = new ConcreteClassB();
+
+                instance.Method();
+                instance.Operation();
+
+                // Delay.
+                Console.ReadKey();
+            }
+        }
+    }
